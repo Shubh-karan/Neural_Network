20e57c126cfb1de5513fd5dc7a12b09f61702fa2
Shubh <shubhkaran255@gmail.com>
2025-08-24 14:33:02 +0530

    Basic Neural Network Perceptron Code for AND Gate

---
+++ main.py
@@ -0,0 +1,13 @@
+def step(x):
+    return 1 if x>=0 else 0
+
+#writting basic perceptron code for AND GATE as it fits on it
+def perceptron(x1,w1,x2,w2,b):
+    y = x1*w1+x2*w2+b
+    return step(y)
+
+#providing optimal values
+print(perceptron(0,0,1,1,-1.5))
+print(perceptron(0,1,1,1,-1.5))
+print(perceptron(1,0,1,1,-1.5))
+print(perceptron(1,1,1,1,-1.5))
